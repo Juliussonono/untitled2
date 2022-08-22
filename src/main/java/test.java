478ba92b178cc18c80b123c482f42e80ac4865ed
juliussonono <sonono26@gmail.com>
2022-08-22 16:16:57 +0200

    hej

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hej");
-        System.out.println("hej");
-        System.out.println("hej");
+        System.out.println("hej hallo");
+
 
 
     }
